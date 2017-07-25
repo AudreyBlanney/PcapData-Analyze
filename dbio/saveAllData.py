1c63ce80e9411f58f10b6f97eb072e17600ce0b7
Audrey <liuxuesong@obsec.net>
2017-07-25 17:35:18 +0800

    change the SQL Table Columns in the same

--- dbio/saveAllData.py
+++ dbio/saveAllData.py
@@ -12,7 +12,7 @@ def getMainDataTableHandle(mySQLDict):
 	sql = 'create database if not exists ' + mySQLDict['database'] +' default charset utf8'
 	cur.execute(sql)
 	conn.select_db(mySQLDict['database']) 
-	sql = 'create table if not exists ' + mySQLDict['mainDataTable'] +"(ID int auto_increment, SRC_IP VARCHAR(32), DES_IP VARCHAR(32), URL text charset utf8, SessionID VARCHAR(255), TITLE text charset utf8, HEAD mediumtext charset utf8, DATE VARCHAR(32), Response mediumtext charset utf8, HTML_BODY mediumtext charset utf8, Submit VARCHAR(8), Submit_value mediumtext charset utf8, primary key(ID))"
+	sql = 'create table if not exists ' + mySQLDict['mainDataTable'] +"(id int auto_increment, src_ip VARCHAR(32), des_ip VARCHAR(32), url text charset utf8, sessionid VARCHAR(255), title text charset utf8, head mediumtext charset utf8, date VARCHAR(32), response mediumtext charset utf8, html_body mediumtext charset utf8,submit VARCHAR(8),submit_value mediumtext charset utf8, primary key(id))"
 
 	cur.execute(sql)
 	return conn, cur
@@ -23,7 +23,7 @@ def saveDatatoMainDB(mySQLDict, dataDict, cur):
 	dataDict['response'] = MySQLdb.escape_string(dataDict['response'])
 	
 	try: 
-		sql = 'insert into '+ mySQLDict['mainDataTable'] + '(SRC_IP, DES_IP, URL, SessionID, TITLE, HEAD, DATE, Response, HTML_BODY, Submit, Submit_value) values(\'%s\', \'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\', \'%s\',\'%s\', \'%s\')' % (dataDict['srcIP'], dataDict['desIP'], dataDict['url'],dataDict['sessionID'],dataDict['title'], dataDict['head'], dataDict['date'], dataDict['response'], dataDict['htmlBody'], dataDict['submit'], dataDict['submitValue'])
+		sql = 'insert into '+ mySQLDict['mainDataTable'] + '(src_ip, des_ip, url, sessionid, title, head, date, response, html_body,submit,submit_value) values(\'%s\', \'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\', \'%s\',\'%s\', \'%s\')' % (dataDict['srcIP'], dataDict['desIP'], dataDict['url'],dataDict['sessionID'],dataDict['title'], dataDict['head'], dataDict['date'], dataDict['response'], dataDict['htmlBody'], dataDict['submit'], dataDict['submitValue'])
 		cur.execute(sql)
 	except MySQLdb.Error,e: 
 		print 'saveAllDate, operation Table Json_Log error! info:%s' % e
